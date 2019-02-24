22e344a86a5696bdebf9b5c65d7fa4ebf7b24f6d
petriken <petriken82@gmail.com>
2019-02-24 16:19:28 +0300

    Updates

---
+++ precache-manifest.ec91ef86f77051c25b21077f5c1dd956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5280a2c0b2d5e5880bdc",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.5280a2c0.js"
+  },
+  {
+    "revision": "9ec068840a748909d089",
+    "url": "/rss-mentor-dashboard/static/js/main.9ec06884.chunk.js"
+  },
+  {
+    "revision": "eedb779d7d83195b5c79",
+    "url": "/rss-mentor-dashboard/static/js/2.eedb779d.chunk.js"
+  },
+  {
+    "revision": "9ec068840a748909d089",
+    "url": "/rss-mentor-dashboard/static/css/main.c21c5e30.chunk.css"
+  },
+  {
+    "revision": "4417849dd2e05de4ab0f93fa305c1881",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
