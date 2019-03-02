1950e31be9c94b04b7ddeb4099bdcf0ee51f1b3c
petriken <petriken82@gmail.com>
2019-03-02 21:51:53 +0300

    Updates

--- precache-manifest.80e8c88c79c251d4338bf2d803380a0a.js
+++ precache-manifest.80e8c88c79c251d4338bf2d803380a0a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rss-mentor-dashboard/static/js/runtime~main.5280a2c0.js"
   },
   {
-    "revision": "8beee1c358e9871b2b55",
-    "url": "/rss-mentor-dashboard/static/js/main.8beee1c3.chunk.js"
+    "revision": "0e5dc195b7c92cb41f3b",
+    "url": "/rss-mentor-dashboard/static/js/main.0e5dc195.chunk.js"
   },
   {
     "revision": "eedb779d7d83195b5c79",
     "url": "/rss-mentor-dashboard/static/js/2.eedb779d.chunk.js"
   },
   {
-    "revision": "8beee1c358e9871b2b55",
-    "url": "/rss-mentor-dashboard/static/css/main.a9b03234.chunk.css"
+    "revision": "0e5dc195b7c92cb41f3b",
+    "url": "/rss-mentor-dashboard/static/css/main.e9aaac6f.chunk.css"
   },
   {
-    "revision": "aa66ed3c571c5cc052a12ed5980de92b",
+    "revision": "e5c8186dec21dbdbcfa3afe174eed09b",
     "url": "/rss-mentor-dashboard/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rss-mentor-dashboard/precache-manifest.73871481db5084159518b8dbbaa8144e.js"
+  "/rss-mentor-dashboard/precache-manifest.80e8c88c79c251d4338bf2d803380a0a.js"
 );
 
 workbox.clientsClaim();
